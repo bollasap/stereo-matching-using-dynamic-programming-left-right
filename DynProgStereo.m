2addd9360ec62a12dfebd9622b3a48d0b1d33375
bollasap <apbollas@hotmail.com>
2025-12-26 11:42:18 +0200

    Add files via upload

--- DynProgStereo.m
+++ DynProgStereo.m
@@ -1,6 +1,6 @@
-occlusionCost = 10;
-discontinuityCost = 3;
 dispLevels = 16;
+Pocc = 10; % Occlusion penalty
+Pdisc = 3; % Vertical discontinuity penalty
 
 % Read stereo image
 left = rgb2gray(imread('Left.png'));
@@ -11,78 +11,71 @@ left = imgaussfilt(left,0.6,'FilterSize',5);
 right = imgaussfilt(right,0.6,'FilterSize',5);
 
 % Get image size
-[height,width] = size(left);
+[rows,cols] = size(left);
 
-C = ones(width,width) * Inf; % Minimal costs
-T = zeros(width,width); % Optimal transitions
-dispMap = zeros(height,width);
+D = Inf(cols+1,cols+1); % Minimum costs
+T = zeros(cols+1,cols+1); % Transitions
+dispMap = zeros(rows,cols);
 
-% For every scanline
-for y=1:height
+% For each scanline
+for y = 1:rows
 
-	% Compute matching cost
-    leftSL = double(left(y,:));
-    rightSL = double(right(y,:));
-    matchingCost = abs(leftSL-rightSL');
-	
+    % Compute matching cost
+    L = double(left(y,:)); % Left scanline
+    R = double(right(y,:)); % Right scanline
+    C = abs(L-R'); % Matching cost
+    
     T0 = T;
 
-	% Forward step
-    if matchingCost(1,1) <= occlusionCost
-        C(1,1) = matchingCost(1,1);
-        T(1,1) = 1;
-    else
-        C(1,1) = occlusionCost;
-        T(1,1) = 2;
-    end
-    C(1,2:dispLevels) = C(1,1) + (1:dispLevels-1) * occlusionCost;
+    % Forward step
+    D(1,1:dispLevels) = (0:dispLevels-1) * Pocc;
     T(1,2:dispLevels) = 2;
-    for x2 = 2:width
-        for x1 = x2:min(x2+dispLevels-1,width)
-			% Compute costs
-            c1 = C(x2-1,x1-1) + matchingCost(x2,x1);
-            c2 = C(x2,x1-1) + occlusionCost;
-            c3 = C(x2-1,x1) + occlusionCost;
+    for j = 2:cols+1
+        for i = j:min(j+dispLevels-1,cols+1)
+            % Compute costs
+            c1 = D(j-1,i-1) + C(j-1,i-1);
+            c2 = D(j,i-1) + Pocc;
+            c3 = D(j-1,i) + Pocc;
 
-			% Add discontinuity cost
-            if T0(x2,x1) == 1
-                c2 = c2 + discontinuityCost;
-                c3 = c3 + discontinuityCost;
-            elseif T0(x2,x1) == 2
-                c1 = c1 + discontinuityCost;
-                c3 = c3 + discontinuityCost;
-            elseif T0(x2,x1) == 3
-                c1 = c1 + discontinuityCost;
-                c2 = c2 + discontinuityCost;
+            % Add discontinuity cost
+            if T0(j,i) == 1
+                c2 = c2 + Pdisc;
+                c3 = c3 + Pdisc;
+            elseif T0(j,i) == 2
+                c1 = c1 + Pdisc;
+                c3 = c3 + Pdisc;
+            elseif T0(j,i) == 3
+                c1 = c1 + Pdisc;
+                c2 = c2 + Pdisc;
             end
 
-			% Find minimal cost
+            % Find minimum cost
             if c1 <= c2 && c1 <= c3
-                C(x2,x1) = c1;
-                T(x2,x1) = 1; % Sequential
+                D(j,i) = c1;
+                T(j,i) = 1; % Match
             elseif c2 <= c3
-                C(x2,x1) = c2;
-                T(x2,x1) = 2; % Occluded
+                D(j,i) = c2;
+                T(j,i) = 2; % Left occlusion
             else
-                C(x2,x1) = c3;
-                T(x2,x1) = 3; % Disoccluded
+                D(j,i) = c3;
+                T(j,i) = 3; % Right occlusion
             end
         end
     end
 
-	% Backtracking
-    x1 = width;
-    x2 = width;
-    while x1 >= 1
-        if T(x2,x1) == 1
-            dispMap(y,x1) = x1-x2;
-            x1 = x1-1;
-            x2 = x2-1;
-        elseif T(x2,x1) == 2
-            %dispMap(y,x1) = x1-x2; % Hide occluded pixels
-            x1 = x1-1;
-        elseif T(x2,x1) == 3
-            x2 = x2-1;
+    % Backtracking
+    i = cols+1;
+    j = cols+1;
+    while i > 1
+        if T(j,i) == 1
+            dispMap(y,i-1) = i-j;
+            i = i-1;
+            j = j-1;
+        elseif T(j,i) == 2
+            %dispMap(y,i-1) = i-j; % disparity map without occlusion
+            i = i-1;
+        elseif T(j,i) == 3
+            j = j-1;
         end
     end
 end
